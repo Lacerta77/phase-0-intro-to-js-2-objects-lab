48929037a9b688444f601baa0cb639a0ea72709c
lacerta <kevinhickman4@gmail.com>
2021-03-20 14:03:05 -0500

    Finished object lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Employee Object
+const employee = {
+    name: 'Kevin',
+    streetAddress: '10 Farms Circle'
+}
+//test 1
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newemployee = {...employee};
+    newemployee[key] = value;
+    return newemployee;
+}
+//test 2
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+//test 3
+function deleteFromEmployeeByKey(employee, key) {
+    const newemployee = { ...employee };
+    delete newemployee[key];
+    return newemployee;
+}
+//test 4
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newemployee = { ...employee };
+    delete employee[key];
+    delete newemployee[key];
+    return employee;
+}
